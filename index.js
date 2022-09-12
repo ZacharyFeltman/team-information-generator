3349555e3a6ea98567416a1ae9c1815b3a8358d2
ZacharyFeltman <zachary.feltman@gmail.com>
2022-09-12 16:27:45 -0600

    fixed tests

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const inquirer = require("inquirer");
 
 const Employee = require("./lib/employee");
@@ -14,7 +15,8 @@ main();
 async function main() {
     // Get manager info
     managerResponse = await inquirer.prompt(Manager.questions);
-    addEmployees();
+    await addEmployees();
+    writeHtmlFile();
 }
 
 async function addEmployees() {
@@ -31,13 +33,13 @@ async function addEmployees() {
         switch(action_response.action) {
             // Add an engineer
             case "Add an engineer":
-                let engineerResponse = await inquirer.prompt(Engineer.questions);
+                var engineerResponse = await inquirer.prompt(Engineer.questions);
                 engineerResponses.push(engineerResponse);
                 break;
 
             // Add an intern
             case "Add an intern":
-                let internResponse = await inquirer.prompt(Intern.questions);
+                var internResponse = await inquirer.prompt(Intern.questions);
                 internResponses.push(internResponse);
                 break;
 
@@ -49,4 +51,76 @@ async function addEmployees() {
     }
 }
 
-// Build the page
+const HTML_HEADER = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/reset.css" />
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Company team information cards</h1>
+    </header>`;
+    
+function writeHtmlFile() {
+    let filename = "myTeam.html";
+    
+    fs.appendFile(filename, HTML_HEADER, () => {});
+    
+    // Write my stuff
+    writeManagerToFile(filename, managerResponse);
+    writeEningeersToFile(filename, engineerResponses);
+    writeInternsToFile(filename, internResponses);
+    
+    fs.appendFile(filename, HTML_FOOTER, () => {});
+}
+
+function writeManagerToFile(filename, managerResponse) {
+    let manager = new Manager(managerResponse.name, managerResponse.email, managerResponse.id, managerResponse.office_number)
+    let managerHtml = `<div class="manager card">
+    Role: ${manager.getRole()} </br>
+    Name: ${manager.getName()} </br?
+    <a href="${manager.getEmail()}">${manager.getEmail()}</a></br>
+    ID: ${manager.getId()}</br>
+    Office number: ${manager.getOfficeNumber()}
+    </div>`;
+    fs.appendFile(filename, managerHtml, () => {});
+}
+
+function writeEningeersToFile(filename, engineerResponses) {
+    console.log("# of engineers: ", engineerResponses.length);
+    for(let i = 0; i < engineerResponses.length; i++) {
+        let engineerResponse = engineerResponses[i];
+        let engineer = new Engineer(engineerResponse.name, engineerResponse.email, engineerResponse.id, engineerResponse.username);
+        let engineerHtml = `<div class="engineer card">
+        Role: ${engineer.getRole()}</br>
+        Name: ${engineer.getName()}</br>
+    <a href="${engineer.getEmail()}">${engineer.getEmail()}</a></br>
+        ID: ${engineer.getId()}
+    <a href="http://www.github.com/${engineer.getGithub()}" target="_blank">GitHub</a>
+</div>`;
+fs.appendFile(filename, engineerHtml, () => {});
+}
+}
+
+function writeInternsToFile(filename, internResponses) {
+    console.log("# of inters: ", internResponses.length);
+    for(let i = 0; i < internResponses.length; i++) {
+        let internResponse = internResponses[i];
+        let intern = new Intern(internResponse.name, internResponse.email, internResponse.id, internResponse.school);
+        let internHtml = `<div class="Intern card">
+        Role: ${intern.getRole()}</br>
+        Name: ${intern.getName()}</br>
+        <a href="${intern.getEmail()}">${intern.getEmail()}</a></br>
+        ID: ${intern.getId()}</br>
+        School: ${intern.getSchool()}
+        </div>`;
+        fs.appendFile(filename, internHtml, () => {});
+    }
+}
+const HTML_FOOTER = `</body>
+</html>`;
--- index.test.js
+++ index.test.js
@@ -1,9 +1,11 @@
+const internal = require("stream");
 const Employee = require("./lib/employee");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 const Manager = require("./lib/manager");
 
 describe("Test employee types: Manager, Engineer & Intern...", () => {
+    // Test the Manager Class
     it("Testing the Manager...", () => {
         const manager = new Manager("Zach", "zach@email.com", "12345", "4");
         expect(manager.getRole()).toBe("Manager");
@@ -11,5 +13,25 @@ describe("Test employee types: Manager, Engineer & Intern...", () => {
         expect(manager.getEmail()).toBe("zach@email.com");
         expect(manager.getId()).toBe("12345");
         expect(manager.getOfficeNumber()).toBe("4");
+    });
+    
+    // Test the Engineer class
+    it("Testing the Engineer...", () => {
+        const engineer = new Engineer("Zach", "zach@email.com", "12345", "ZachFeltman");
+        expect(engineer.getRole()).toBe("Engineer");
+        expect(engineer.getName()).toBe("Zach");
+        expect(engineer.getEmail()).toBe("zach@email.com");
+        expect(engineer.getId()).toBe("12345");
+        expect(engineer.getGithub()).toBe("ZachFeltman");
+    });
+
+    // Test the Intern Class
+    it("Testing the Intern...", () => {
+        const intern = new Intern("Zach", "zach@email.com", "12345", "4");
+        expect(intern.getRole()).toBe("Intern");
+        expect(intern.getName()).toBe("Zach");
+        expect(intern.getEmail()).toBe("zach@email.com");
+        expect(intern.getId()).toBe("12345");
+        expect(intern.getSchool()).toBe("4");
     });
 });
